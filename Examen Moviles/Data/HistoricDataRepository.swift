e5669f408f16436329dcb7fa0d5a47ef3658e495
ELECTRIC80008STORM <63619664+ELECTRIC80008STORM@users.noreply.github.com>
2024-11-21 13:38:49 -0600

    Add the Historic Data Repository

--- Examen Moviles/Data/HistoricDataRepository.swift
+++ Examen Moviles/Data/HistoricDataRepository.swift
@@ -1,3 +1,19 @@
 import Foundation
 
+protocol HistoricDataRepositoryProtocol {
+    func getHistoricData() async throws -> HistoricDataList
+}
 
+class HistoricDataRepository: HistoricDataRepositoryProtocol {
+    static let shared = HistoricDataRepository()
+    
+    let nservice: ParseService
+    
+    private init(nservice: ParseService = ParseService.shared) {
+        self.nservice = nservice
+    }
+    
+    func getHistoricData() async throws -> HistoricDataList {
+        return try await nservice.getHistoricData()
+    }
+}
