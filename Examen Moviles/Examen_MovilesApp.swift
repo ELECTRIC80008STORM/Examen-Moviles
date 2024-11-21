7b12e0881f31bb07d5ebdaded0c8f8eca4b4ebd3
ELECTRIC80008STORM <63619664+ELECTRIC80008STORM@users.noreply.github.com>
2024-11-21 09:44:47 -0600

    Initial Commit

---
+++ Examen Moviles/Examen_MovilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Examen_MovilesApp.swift
+//  Examen Moviles
+//
+//  Created by Rommel Toledo Crespo on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Examen_MovilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Examen MovilesTests/Examen_MovilesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Examen_MovilesTests.swift
+//  Examen MovilesTests
+//
+//  Created by Rommel Toledo Crespo on 21/11/24.
+//
+
+import Testing
+@testable import Examen_Moviles
+
+struct Examen_MovilesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
